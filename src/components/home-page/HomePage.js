f99edd6e46620cb4bcbd576d69c3334599d76126
dennis <dgyurics@gmail.com>
2018-09-01 23:43:02 -0700

    convert home page to stateless component

--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -1,24 +1,18 @@
-import React, { Component, PropTypes } from 'react'
+import React from 'react'
 import Header from './header/Header.js'
 import Footer from './footer/Footer.js'
 import styles from './HomePage.css'
 
-class Home extends Component {
-  constructor(state, props) {
-    super(state, props);
-  }
-
-  render() {
-    return (
-      <div className={styles.container}>
-        <Header/>
-        <h1 className={styles.header}>
-          A <span className={styles.headerlrg}>life</span> well lived
-        </h1>
-        <Footer/>
-      </div>
-    );
-  }
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <Header/>
+      <h1 className={styles.header}>
+        A <span className={styles.headerlrg}>life</span> well lived
+      </h1>
+      <Footer/>
+    </div>
+  );
 }
 
 export default Home;
