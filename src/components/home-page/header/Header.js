33474d89ccc832bfb2dce577a1a33f3037963b6b
dennis <dgyurics@gmail.com>
2018-09-01 21:13:22 -0700

    remove unused files + remove github link

--- src/components/home-page/header/Header.js
+++ src/components/home-page/header/Header.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from './Header.css'
 import { Icon } from 'react-icons-kit'
 import { messageCircle } from 'react-icons-kit/feather/messageCircle'
-import { github } from 'react-icons-kit/feather/github'
 import { layers } from 'react-icons-kit/feather/layers'
 import { user } from 'react-icons-kit/feather/user'
 import { Link } from 'react-router-dom'
@@ -11,11 +10,6 @@ const Header = props => {
   return (
     <div className={styles.header}>
       <ol>
-        <li>
-          <a href="https://github.com/dgyurics">
-            <Icon icon={github}/>
-          </a>
-        </li>
         <li>
           <Link to={'/idea'}>
             <Icon icon={layers}/>
