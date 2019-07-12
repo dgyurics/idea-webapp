2a1b032b57c3c437ae86a4b55cda8acdc028b774
Dennis Gyurics <dgyurics@gmail.com>
2019-07-12 01:04:01 -0700

    modal enhancements + utilize ReactDom.Portal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './Modal.css';
 
 class Modal extends Component {
   handleClose = (event) => {
-    event.preventDefault();
     const { onClose } = this.props;
     if (event.target === event.currentTarget && onClose) {
       onClose();
@@ -15,14 +15,15 @@ class Modal extends Component {
     const { children, toggle } = this.props;
     const modalVisible = toggle ? 'modal__container modal__container--visible'
       : 'modal__container';
-    return (
+    return ReactDOM.createPortal(
       <div onClick={this.handleClose} onKeyDown={this.handleClose} className={modalVisible}>
         <div className="modal">
           <div className="modal__content">
             {children}
           </div>
         </div>
-      </div>
+      </div>,
+      document.body,
     );
   }
 }
