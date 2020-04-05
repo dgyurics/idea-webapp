168b381c8914afe4319a01f9115f51306b0f7dda
Dennis Gyurics <dgyurics@gmail.com>
2020-04-05 11:35:58 -0700

    update resource page

--- src/components/resource-page/ResourcePage.jsx
+++ src/components/resource-page/ResourcePage.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import NavBar from '../navigation/Navigation';
-import { BookProvider } from '../context/BookContext';
-import Books from './books/Books';
 import './ResourcePage.css';
 
 const ResourcePage = () => (
   <div className="resource-page">
     <NavBar />
     <div className="resource-page__container">
-      <div className="resource-page__main">
-        <BookProvider>
-          <Books />
-        </BookProvider>
-      </div>
+      <div className="resource-page__main" />
     </div>
     <div className="resource-footer no-touch">
       <span>Coming soon</span>
