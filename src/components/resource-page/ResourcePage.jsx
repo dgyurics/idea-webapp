91dd8c8d0f4cf2b758fbc76f6c323bb3c0c6fdb8
Dennis Gyurics <dgyurics@gmail.com>
2020-04-20 12:47:05 -0700

    Revert "update resource page"
    This reverts commit 168b381c8914afe4319a01f9115f51306b0f7dda.

--- src/components/resource-page/ResourcePage.jsx
+++ src/components/resource-page/ResourcePage.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import NavBar from '../navigation/Navigation';
+import { BookProvider } from '../context/BookContext';
+import Books from './books/Books';
 import './ResourcePage.css';
 
 const ResourcePage = () => (
   <div className="resource-page">
     <NavBar />
     <div className="resource-page__container">
-      <div className="resource-page__main" />
+      <div className="resource-page__main">
+        <BookProvider>
+          <Books />
+        </BookProvider>
+      </div>
     </div>
     <div className="resource-footer no-touch">
       <span>Coming soon</span>
