8bfb36b400524364e5e4243bfb63cb2df3fa2c9e
Dennis Gyurics <dgyurics@gmail.com>
2020-08-04 16:47:11 -0700

    navigation overhaul

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,56 +1,29 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Icon } from 'react-icons-kit';
-import { menu } from 'react-icons-kit/feather';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
 
-class Navigation extends Component {
-  state = {
-    visible: false,
-  };
-
-  onClick = () => {
-    this.setState(prevState => ({ visible: !prevState.visible }));
-  }
-
-  render = () => {
-    const { visible } = this.state;
-    const { whiteBackground } = this.props;
-    const navigationClass = visible ? 'navigation' : 'navigation navigation--hidden';
-    const menuClass = whiteBackground ? 'navigation__menu-icon navigation__menu-icon--black' : 'navigation__menu-icon';
-    return (
-      <div className={navigationClass}>
-        <div className="navigation__menu">
-          <ol className="navigation__menu__items no-touch">
-            <Link to="/resource" className="navigation__menu__link">
-              <li className="navigation__menu__item">
-                resources
-              </li>
-            </Link>
-            <Link to="/contact" className="navigation__menu__link">
-              <li className="navigation__menu__item">
-                contact
-              </li>
-            </Link>
-          </ol>
-        </div>
-        <div className="navigation__toggle">
-          <div className="navigation__icon__container">
-            <Icon size="100%" style={{ display: 'inline' }} icon={menu} onMouseDown={this.onClick} className={menuClass} />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-Navigation.propTypes = {
-  whiteBackground: PropTypes.bool,
-};
-
-Navigation.defaultProps = {
-  whiteBackground: true,
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <ol className="navigation__menu no-touch">
+        <Link to="/resource" className="navigation__menu__link">
+          <li className="navigation__menu__item">
+            product
+          </li>
+        </Link>
+        <Link to="/resource" className="navigation__menu__link">
+          <li className="navigation__menu__item">
+            excerpt
+          </li>
+        </Link>
+        <Link to="/contact" className="navigation__menu__link">
+          <li className="navigation__menu__item navigation__menu__item--last">
+            about
+          </li>
+        </Link>
+      </ol>
+    </div>
+  );
 };
 
 export default Navigation;
