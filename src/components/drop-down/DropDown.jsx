8e6ab37fc41d283b2e6d298b880c85d0fae75220
Dennis Gyurics <dgyurics@gmail.com>
2020-09-06 22:24:54 -0700

    css change dropdown

--- src/components/drop-down/DropDown.jsx
+++ src/components/drop-down/DropDown.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Icon } from 'react-icons-kit';
 import PropTypes from 'prop-types';
 import { moreHorizontal } from 'react-icons-kit/feather';
@@ -6,9 +6,21 @@ import './DropDown.css';
 
 const DropDown = ({ actions }) => {
   const [visible, toggleVisibility] = useState(false);
+  const ref = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      toggleVisibility(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, false);
+    return () => document.removeEventListener('click', handleClickOutside, false);
+  });
 
   return (
-    <div className="drop-down">
+    <div className="drop-down" ref={ref}>
       <div className="drop-down__icon" onClick={() => toggleVisibility(!visible)}>
         <Icon className="edit-product-icon" icon={moreHorizontal} />
       </div>
